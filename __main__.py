61009097c0fc6ae18057e8eba6b318d0c5a0146c
[BKOPYZ] <[breakoff1530@gmail,com]>
2023-08-20 21:43:55 +0700

    feat: Add helper_func.py

---
+++ __main__.py
@@ -0,0 +1,3 @@
+import helper_func
+if __name__ == "__main__":
+    helper_func.get_all_file_path("asdf")
---
+++ helper_func.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import os
+from typing import List,Tuple, Dict
+
+def get_all_file_path(path: Path) -> List:
+    """
+    Check if the path has child files/folder. Glob it and (append it to the list).
+    Repeat until no more child.
+
+    Param path: path of the file or folder
+
+    Return list of string that contains all the path inside
+    """
+    all_file_path_list = []
+    # Check if the path valid (is file or directory)
+    if not path.exists():
+        raise OSError("file or directory not exist. Please check the path again")
+    
+    return all_file_path_list
+
+
+def get_file_size(path: Path) -> Tuple:
+    """
+    """
+    
+
+
+
+    
+        
