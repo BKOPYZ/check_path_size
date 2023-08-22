250b4389239fa99843945dfddf54c94ee0cb5d47
[BKOPYZ] <[breakoff1530@gmail,com]>
2023-08-22 22:45:45 +0700

    feat: change the format of the log & add log's file name for cleaner look

--- helper_func.py
+++ helper_func.py
@@ -13,7 +13,6 @@ CONFIG_PATH = "config.txt"
 # --------------------------------------------------
 def set_pathinfo(path: Path, depth: int, ) -> None:
     pathfile = Path(path)
-    # Check if the path valid (is file or directory)
     if not pathfile.exists():
         raise OSError(
             "file or directory not exist. Please check the path again")
@@ -31,10 +30,10 @@ def __get_directory_data(pathfile: Path, pos: int, depth: int) -> Tuple[List[Pat
     for pathfile in tqdm(allfilepath):
         pathprop = __get_path_properties(pathfile,pos,depth)
         path_properties_list.extend(pathprop)
-        directory_size += pathprop[0].get_size() 
+        directory_size += pathprop[0].get_uncalculated_size() 
     return (path_properties_list,directory_size)
 
-def __get_directory_size_depth_reach(pathfile: str)->float:
+def __get_directory_size_depth_reach(pathfile: str)->float: # TODO: fix this shit
     size = 0.0
     pathfile = Path(pathfile)
     for path, dirs, files in os.walk(pathfile):
@@ -49,30 +48,31 @@ def __add_pathinfo(pathPropertieslist: List[PathProperties])-> None:
         pathinfo += path_object()
 
 
-def __get_path_properties(pathfile: Path, pos: int, depth: int) -> List[PathProperties]:
+def __get_path_properties(pathfile: Path, pos: int, depth: int) -> List[PathProperties]: # TODO: fix this shit
     """
     """
     global size
     if Path(pathfile).is_file():
         size_of_file = os.path.getsize(pathfile)
         size += size_of_file
-        return [PathProperties(pathfile, size_of_file, PathUnit.BYTE, PathType.FILE)]
+        return [PathProperties(pathfile, size_of_file, PathType.FILE)]
     elif pos < depth:
         path_properties_list, directory_size = __get_directory_data(pathfile,pos+1,depth)
-        pathprop = [PathProperties(pathfile,directory_size , PathUnit.BYTE,PathType.DIRECTORY)]
+        pathprop = [PathProperties(pathfile,directory_size , PathType.DIRECTORY)]
         pathprop.extend(path_properties_list)
         return pathprop
     else:
         directory_size = __get_directory_size_depth_reach(pathfile)
-        return [PathProperties(pathfile,directory_size, PathUnit.BYTE, PathType.DIRECTORY)]
+        size += directory_size
+        return [PathProperties(pathfile,directory_size, PathType.DIRECTORY)]
         
 def set_title(pathfile: str, size: float) -> None:
-    fileTitle = PathProperties(pathfile, size,PathUnit.BYTE,PathType.DIRECTORY)
+    fileTitle = PathProperties(pathfile, size,PathType.TITLE)
     global pathinfo
     pathinfo = fileTitle() + pathinfo
 
-def write_log() -> None:
-    with open("log.txt", 'w', encoding="utf-8") as f:
+def write_log(path:str) -> None:
+    with open(".\log\log_" + path.split("\\")[-1]+ ".txt", 'w', encoding="utf-8") as f:
         f.write(pathinfo)
 
 
@@ -93,10 +93,17 @@ def get_data_from_config(path: str = CONFIG_PATH) -> Tuple[str,int]:
         else:     
             depth = datas[1]
         path_to_check = datas[0]
-    return (path_to_check, depth)        
-    
-    
+    return (path_to_check, depth)
 
+def check_path_log(path:str)->bool:
+    path = ".\log\log_" + path.split("\\")[-1] +".txt"
+    if os.path.isfile(path):
+        return True;
+    return False
+    
+def edit_path_log(path:str)->str:
+    suffix_num = 0 if path.split("\\")[-1][-1].isalpha() else int(path.split("\\")[-1][-1]) +1
+    return path + str(suffix_num)
 
 def run() -> None:
     global size, pathinfo
@@ -105,13 +112,18 @@ def run() -> None:
     try:
         path_to_check, depth = get_data_from_config()
         depth = int(depth)
+        path = path_to_check
+        if check_path_log(path):
+            while((ans := input("Do you want to replace the file that already exist? (Y/n): ").lower()) not in ['y','n']): pass
+            if ans == "n":
+                path = edit_path_log(path)
         PathProperties.set_smallestindex(path_to_check)
         set_pathinfo(path_to_check,depth)
         set_title(path_to_check, size)
-        write_log()
+        write_log(path)
     except Exception as e:
         print(e)
-        print(e.with_traceback(None))
+        print(e.with_traceback())
         
     
     
