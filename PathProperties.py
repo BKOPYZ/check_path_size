d8c333384b74e577026c7519030669a7c181f5e5
[BKOPYZ] <[breakoff1530@gmail,com]>
2023-08-23 13:14:32 +0700

    feat: make it compatable for linux fix bug

--- PathProperties.py
+++ PathProperties.py
@@ -1,26 +1,27 @@
 from enum import Enum
 from math import floor
 
-#TODO: fix the bug that happen from assigning the pathunit and convert the size before calculate the size of the directory
 
 class PathUnit(Enum):
     BYTE = 3
     KILO = 2
     MEGA = 1
     GIGA = 0
-    
+
 
 class PathType(Enum):
-    FILE =0
-    DIRECTORY = 1 
-    TITLE =2
+    FILE = 0
+    DIRECTORY = 1
+    TITLE = 2
+
 
 class PathProperties:
     BYTE_TO_KILOBYTE = 1.0/1024
     BYTE_TO_MEGABYTE = 1.0/(1024 ** 2)
     BYTE_TO_GIGABYTE = 1.0/(1024 ** 3)
-    smallestindex = None;
+    smallestindex = None
     # next time will do the file and folder count the check the similarity next time
+
     def __init__(self, path: str, size: float, pathtype: PathType = PathType.TITLE):
         self.unit = PathUnit.BYTE
         self.type = pathtype
@@ -29,15 +30,15 @@ class PathProperties:
         self.__uncalculated_size = size
         self.__name = self.__set_name(path)
         self.__size = self.__set_size(size)
-        
-        
+
     def __call__(self) -> str:
         spacebet = 95 + 3-len(str(floor(self.get_size())))
-        spacefront = "\t" * self.pos + ("┗╸" if self.type == PathType.TITLE else "┣╸" )
+        spacefront = "\t" * self.pos + \
+            ("┗╸" if self.type == PathType.TITLE else "┣╸")
         suffix = self.suffix_ended()
-        return  f"{spacefront}{self.get_name():<{spacebet}} {self.get_size():.2f} {suffix}\n"
+        return f"{spacefront}{self.get_name():<{spacebet}} {self.get_size():.2f} {suffix}\n"
 
-    def suffix_ended(self)->str:
+    def suffix_ended(self) -> str:
         match self.unit:
             case PathUnit.KILO:
                 return "KB"
@@ -50,25 +51,25 @@ class PathProperties:
 
     def get_name(self) -> str:
         return self.__name
-    
+
     def get_size(self) -> float:
         return self.__size
-    
-    def get_uncalculated_size(self)->float:
+
+    def get_uncalculated_size(self) -> float:
         return self.__uncalculated_size
-    
+
     def get_path(self) -> str:
         return self.__path
-    
-    def __set_name(self, path:str) -> str:
-        splited_file = str(path).replace("/","\\").split("\\")
+
+    def __set_name(self, path: str) -> str:
+        splited_file = str(path).replace("/", "\\").split("\\")
         name = splited_file[-1] if not splited_file[-1].isspace() else splited_file[-2]
         if len(name) >= 20:
             name = name[:20] + "..."
         return name
-    
+
     def __set_size(self, size: float) -> float:
-        if size > 1000000000 : 
+        if size > 1000000000:
             self.unit = PathUnit.GIGA
             return size * PathProperties.BYTE_TO_GIGABYTE
         elif size > 1000000:
@@ -78,18 +79,11 @@ class PathProperties:
             self.unit = PathUnit.KILO
             return size * PathProperties.BYTE_TO_KILOBYTE
         else:
-            return size 
-        
+            return size
+
     def __set_pos(self, path: str) -> int:
         return len(str(path).split("\\")) - self.smallestindex
-     
-    @classmethod
-    def set_smallestindex(cls,path: str):
-        cls.smallestindex = len(path.split('\\'))
-       
-    
-    
-    
-    
 
-        
+    @classmethod
+    def set_smallestindex(cls, path: str):
+        cls.smallestindex = len(path.split('\\'))
--- helper_func.py
+++ helper_func.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import os
 from typing import List, Tuple, Dict
-from PathProperties import PathProperties,PathType, PathUnit
+from PathProperties import PathProperties, PathType, PathUnit
 from tqdm import tqdm
 from timeit import Timer
-from CustomError import InvalidUrlException 
+from CustomError import InvalidUrlException
 import math
 import sys
 
@@ -12,6 +12,8 @@ import sys
 CONFIG_PATH = "config.txt"
 
 # --------------------------------------------------
+
+
 def set_pathinfo(path: Path, depth: int, ) -> None:
     pathfile = Path(path)
     if not pathfile.exists():
@@ -23,33 +25,38 @@ def set_pathinfo(path: Path, depth: int, ) -> None:
         fileprop = __get_path_properties(pathfile, 0, depth)
         __add_pathinfo(fileprop)
 
-def __get_directory_data(pathfile: Path, pos: int, depth: int) -> Tuple[List[PathProperties],int]:
+
+def __get_directory_data(pathfile: Path, pos: int, depth: int) -> Tuple[List[PathProperties], int]:
     directory_size = 0.0
     pathfile = Path(pathfile)
-    path_properties_list:List[PathProperties] = []
+    path_properties_list: List[PathProperties] = []
     allfilepath = list(pathfile.glob("*"))
     for pathfile in tqdm(allfilepath):
-        pathprop = __get_path_properties(pathfile,pos,depth)
+        pathprop = __get_path_properties(pathfile, pos, depth)
         path_properties_list.extend(pathprop)
-        directory_size += pathprop[0].get_uncalculated_size() 
-    return (path_properties_list,directory_size)
+        directory_size += pathprop[0].get_uncalculated_size()
+    return (path_properties_list, directory_size)
 
-def __get_directory_size_depth_reach(pathfile: str)->float: # TODO: fix this shit
+
+# TODO: fix this shit
+def __get_directory_size_depth_reach(pathfile: str) -> float:
     size = 0.0
     pathfile = Path(pathfile)
     for path, dirs, files in os.walk(pathfile):
         for file in files:
-            joined_path = os.path.join(path,file)
+            joined_path = os.path.join(path, file)
             size += os.path.getsize(joined_path)
     return size
-        
-def __add_pathinfo(pathPropertieslist: List[PathProperties])-> None:
+
+
+def __add_pathinfo(pathPropertieslist: List[PathProperties]) -> None:
     global pathinfo
     for path_object in pathPropertieslist:
         pathinfo += path_object()
 
 
-def __get_path_properties(pathfile: Path, pos: int, depth: int) -> List[PathProperties]: # TODO: fix this shit
+# TODO: fix this shit
+def __get_path_properties(pathfile: Path, pos: int, depth: int) -> List[PathProperties]:
     """
     """
     global size
@@ -58,31 +65,36 @@ def __get_path_properties(pathfile: Path, pos: int, depth: int) -> List[PathProp
         size += size_of_file
         return [PathProperties(pathfile, size_of_file, PathType.FILE)]
     elif pos < depth:
-        path_properties_list, directory_size = __get_directory_data(pathfile,pos+1,depth)
-        pathprop = [PathProperties(pathfile,directory_size , PathType.DIRECTORY)]
+        path_properties_list, directory_size = __get_directory_data(
+            pathfile, pos+1, depth)
+        pathprop = [PathProperties(
+            pathfile, directory_size, PathType.DIRECTORY)]
         pathprop.extend(path_properties_list)
         return pathprop
     else:
         directory_size = __get_directory_size_depth_reach(pathfile)
         size += directory_size
-        return [PathProperties(pathfile,directory_size, PathType.DIRECTORY)]
-        
+        return [PathProperties(pathfile, directory_size, PathType.DIRECTORY)]
+
+
 def set_title(pathfile: str, size: float) -> None:
-    fileTitle = PathProperties(pathfile, size,PathType.TITLE)
+    fileTitle = PathProperties(pathfile, size, PathType.TITLE)
     global pathinfo
     pathinfo = fileTitle() + pathinfo
 
-def write_log(path:str) -> None:
+
+def write_log(path: str) -> None:
     with open(path, 'w', encoding="utf-8") as f:
         f.write(pathinfo)
 
 
-def check_valid_path(path: str)-> bool:
+def check_valid_path(path: str) -> bool:
     if os.path.exists(path):
         return True
     return False
 
-def get_data_from_config(path: str = CONFIG_PATH) -> Tuple[str,int]:
+
+def get_data_from_config(path: str = CONFIG_PATH) -> Tuple[str, int]:
     depth = 1000
     with open(CONFIG_PATH, 'r') as config:
         datas = config.read().splitlines()
@@ -91,35 +103,43 @@ def get_data_from_config(path: str = CONFIG_PATH) -> Tuple[str,int]:
         elif len(datas) == 1:
             if not check_valid_path(path):
                 raise FileNotFoundError("[ERROR]: File or Directory not found")
-        else:     
+        else:
             depth = datas[1]
         path_to_check = datas[0]
     return (path_to_check, depth)
 
-def check_path_log(path:str)->bool:
+
+def check_path_log(path: str) -> bool:
     if os.path.isfile(path):
-        return True;
+        return True
     return False
-    
-def edit_path_log(path:str)->str:
+
+
+def edit_path_log(path: str) -> str:
     if platform_is_window():
-        suffix_num = 0 if path.split("\\")[-1].split('.')[0][-1].isalpha() else int(path.split("\\")[-1].split(".")[0][-1]) +1
+        suffix_num = 0 if path.split(
+            "\\")[-1].split('.')[0][-1].isalpha() else int(path.split("\\")[-1].split(".")[0][-1]) + 1
     else:
-        suffix_num = 0 if path.split("/")[-1].split('.')[0][-1].isalpha() else int(path.split("/")[-1].split(".")[0][-1]) +1
-    while(check_path_log(path+str(suffix_num))):
-        suffix_num+=1
+        suffix_num = 0 if path.split(
+            "/")[-1].split('.')[0][-1].isalpha() else int(path.split("/")[-1].split(".")[0][-1]) + 1
+    while (check_path_log(path+str(suffix_num))):
+        suffix_num += 1
     return path + str(suffix_num)
 
-def norm_path(path:str)->str:
-    return path.replace("/","\\")
 
-def platform_is_window()->bool:
-    return sys.platform == "Windows"
+def norm_path(path: str) -> str:
+    return path.replace("/", "\\")
 
-def get_path_log(path:str)->str:
+
+def platform_is_window() -> bool:
+    return sys.platform == "win32"
+
+
+def get_path_log(path: str) -> str:
     if platform_is_window():
         return ".\log\log_" + path.split("\\")[-1] + ".txt"
     return "./log/log_" + path.split("\\")[-1] + ".txt"
+
 
 def run() -> None:
     global size, pathinfo
@@ -130,17 +150,14 @@ def run() -> None:
         depth = int(depth)
         path = get_path_log(norm_path(path_to_check))
         if check_path_log(path):
-            while((ans := input("Do you want to replace the file that already exist? (Y/n): ").lower()) not in ['y','n']): pass
+            while ((ans := input("Do you want to replace the file that already exist? (Y/n): ").lower()) not in ['y', 'n']):
+                pass
             if ans == "n":
                 path = edit_path_log(path)
         PathProperties.set_smallestindex(path_to_check)
-        set_pathinfo(path_to_check,depth)
+        set_pathinfo(path_to_check, depth)
         set_title(path_to_check, size)
         write_log(path)
     except Exception as e:
         print(e)
         print(e.with_traceback())
-        
-    
-    
-    
